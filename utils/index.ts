a1abeade4b5791f50786338d10b824c044641303
Tarek Ahmad <t.ahmad@work-with-nerds.ca>
2023-08-16 18:02:52 +0300

    get car images from an API

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { CarProps } from "@/types";
 
 export async function fetchCars() {
 	const headers = {
@@ -5,7 +6,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-	const responce = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+	const responce = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
 		headers: headers
 	});
 
@@ -27,3 +28,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
 	return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+	const url = new URL("https://cdn.imagin.studio/getimage");
+	const { make, model, year } = car;
+
+	url.searchParams.append('customer', 'hrjavascript-mastery');
+	url.searchParams.append('make', make);
+	url.searchParams.append('modelFamily', model.split(" ")[0]);
+	url.searchParams.append('zoomType', 'fullscreen');
+	url.searchParams.append('modelYear', `${year}`);
+	url.searchParams.append('angle', `${angle}`);
+
+	return `${url}`;
+}
