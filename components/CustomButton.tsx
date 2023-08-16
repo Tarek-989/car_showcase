2cb2b544aa8dcd796a700839d064820d5d1bdaab
Tarek Ahmad <t.ahmad@work-with-nerds.ca>
2023-08-16 11:59:10 +0300

    add rightIcon & textStyle to CustomButton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,7 +2,7 @@
 import { CustomButtonProps } from '@/types';
 import Image from 'next/image';
 
-export const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+export const CustomButton = ({ title, containerStyles, handleClick, btnType, rightIcon, textStyle, isDisabled }: CustomButtonProps) => {
 	return (
 		<button
 			disabled={false}
@@ -10,9 +10,19 @@ export const CustomButton = ({ title, containerStyles, handleClick, btnType }: C
 			className={`custom-btn ${containerStyles}`}
 			onClick={handleClick}
 		>
-			<span className={'flex-1'}>
+			<span className={`flex-1 ${textStyle}`}>
 				{title}
 			</span>
+			{rightIcon && (
+				<div className='relative w-6 h-6'>
+					<Image
+						src={rightIcon}
+						alt='right icon'
+						fill
+						className='object-contain'
+					/>
+				</div>
+			)}
 		</button>
 	)
 }
