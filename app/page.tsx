c2bf2be7e327b56568fb34ac95435ee314ed8c74
Tarek Ahmad <t.ahmad@work-with-nerds.ca>
2023-08-16 12:01:15 +0300

    show all cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { CustomFilter, Hero, SerachBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SerachBar } from '@/components'
+import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+	const allCars = await fetchCars()
+	const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 	return (
 		<main className="overflow-hidden">
 			<Hero />
@@ -13,10 +16,25 @@ export default function Home() {
 				<div className='home__filters'>
 					<SerachBar />
 					<div className='home__filter-container'>
-						 <CustomFilter title={'fuel'}/>
-						 <CustomFilter title={'year'}/>
+						<CustomFilter title={'fuel'} />
+						<CustomFilter title={'year'} />
 					</div>
 				</div>
+				{!isDataEmpty ? (
+					<section>
+						<div className='home__cars-wrapper'>
+							{allCars.map((car) => (
+								<CarCard car={car} />
+							))}
+						</div>
+					</section>
+				) : (
+					<div className='home__error-container'>
+						<h2 className='text-black text-xl font-bold'>Opps, no results</h2>
+						<p>{allCars?.message}</p>
+					</div>
+				)}
+
 			</div>
 		</main>
 	)
