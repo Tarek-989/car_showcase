6a4859ad8dfea47ed6da851dcd8ac931ed858a95
Tarek Ahmad <t.ahmad@work-with-nerds.ca>
2023-08-17 13:14:46 +0300

    handle search inputs

--- components/SerachBar.tsx
+++ components/SerachBar.tsx
@@ -1,10 +1,53 @@
 'use client';
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { SearchManufacturer } from './SearchManufacturer';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
+const SerachButton = ({ otherClasses }: { otherClasses: string }) => (
+	<button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+		<Image
+			src={'/magnifying-glass.svg'}
+			alt='magnifying glass'
+			width={40}
+			height={40}
+			className='object-contain'
+		/>
+	</button>
+)
 export const SerachBar = () => {
 	const [manufacturer, setManufacturer] = useState('')
-	const handleSubmit = () => { };
+	const [model, setModel] = useState('')
+	const router = useRouter()
+
+	const updateSearchParams = (model: string, manufacturer: string) => {
+		const searchParams = new URLSearchParams(window.location.search)
+
+		if (model) {
+			searchParams.set('model', model)
+		} else {
+			searchParams.delete('model')
+		}
+		if (manufacturer) {
+			searchParams.set('manufacturer', manufacturer)
+		} else {
+			searchParams.delete('manufacturer')
+		}
+
+		const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+		router.push(newPathname)
+	}
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (manufacturer === '' && model === '') {
+			return alert('Please fill in the search bar')
+		}
+
+		updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+	};
 
 	return (
 		<form className='searchbar' onSubmit={handleSubmit}>
@@ -13,7 +56,27 @@ export const SerachBar = () => {
 					manufacturer={manufacturer}
 					setManufacturer={setManufacturer}
 				/>
+				<SerachButton otherClasses='sm:hidden' />
 			</div>
+			<div className='searchbar__item'>
+				<Image
+					src={'/model-icon.png'}
+					width={25}
+					height={25}
+					className='absolute w-[20px] h-[20px] ml-4'
+					alt='car model'
+				/>
+				<input
+					type='text'
+					name='model'
+					value={model}
+					onChange={(e) => setModel(e.target.value)}
+					placeholder='Tiguen'
+					className='searchbar__input'
+				/>
+				<SerachButton otherClasses='sm:hidden' />
+			</div>
+			<SerachButton otherClasses='max-sm:hidden' />
 		</form>
 	)
 }
